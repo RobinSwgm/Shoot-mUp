9d5e22f8b38dfa3479b6d519a9508b6adae8f2b8
RobinSwgm <robin.swagerman@gmail.com>
2024-10-09 11:52:12 +0200

    PlayerInputsWorks

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,27 +1,57 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float movementSpeed = 1f;
+    public Rigidbody2D rb;
+    public float moveSpeed = 5f;
+    public InputAction playerControls;
+    public InputAction playershoot;
     [SerializeField] private GameObject bulletPrefab;
+    private bool shot;
 
-public void Update()
-{
-    float direction = Input.GetAxisRaw("Horizontal");
+    Vector2 moveDirection = Vector2.zero;
 
-    transform.Translate(direction * Time.deltaTime * movementSpeed, 0, 0);
-
-    if (Input.GetKeyDown(KeyCode.Space))
+    private void OnEnable()
     {
-        Shoot();
+        playerControls.Enable();
+        playershoot.Enable();
     }
-}
-void Shoot()
-{
-    Instantiate(bulletPrefab, transform.position , transform.rotation);
-}
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+        playershoot.Disable();
+    }
+
+    private void Update()
+    {
+        moveDirection = playerControls.ReadValue<Vector2>();
+        if (playershoot.ReadValue<float>() > 0f && !shot)
+        {
+            StartCoroutine(ShootBullet());
+            //Shoot();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+    IEnumerator ShootBullet()
+    {
+        shot = true;
+        Instantiate(bulletPrefab, transform.position, transform.rotation);
+        yield return new WaitForSeconds(0.1f);
+        shot = false;
+    }
+
+    void Shoot()
+    {
+       
+    }
 
 }
